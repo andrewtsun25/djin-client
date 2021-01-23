5df5571170a29e6594c77567d23ce05d3aabccbe
Andrew Tsun <atsun@usc.edu>
2021-01-23 12:17:06 -0800

    Add ordering to education elements

--- src/hooks/useEducation.ts
+++ src/hooks/useEducation.ts
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
 import {Education, EduType} from "types/education";
 import {CodingEducation, MusicEducation} from "const/data/education";
+import { orderBy } from "lodash";
 
 export default function useEducations(category: string): Education[] {
     const [educations, setEducations] = useState<Education[]>([]);
     useEffect(() => {
         switch(category) {
             case EduType.CODING:
-                setEducations(CodingEducation);
+                setEducations(orderBy(CodingEducation, ['endDate'], ['desc']));
                 break;
             case EduType.MUSIC:
-                setEducations(MusicEducation);
+                setEducations(orderBy(MusicEducation, ['endDate'], ['desc']));
                 break;
             default:
                 setEducations([]);
