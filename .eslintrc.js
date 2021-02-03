b99b433091ac707ae3772d87a3dc4c9d4a25d4f5
Andrew Tsun <atsun@usc.edu>
2021-02-02 23:02:49 -0800

    Add typescript-eslint as plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    plugins: ['@typescript-eslint'],
     parserOptions: {
         ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
         sourceType: 'module', // Allows for the use of imports
