dca2c2a2f8cc085d5e43d75e4b3f5e1da4380e98
Andrew Tsun <atsun@usc.edu>
2021-02-18 22:51:37 -0800

    Migrate Music Scores to GCP Storage

--- src/components/AppContent/MusicScoresPage/MusicScoresPage.tsx
+++ src/components/AppContent/MusicScoresPage/MusicScoresPage.tsx
@@ -1,6 +1,5 @@
 import { Container, Typography, useMediaQuery } from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
-import musicScoreBg from 'assets/music/media/music_score_bg.jpeg';
 import Background from 'components/shared/Background';
 import useMusicScores from 'hooks/useMusicScores';
 import React from 'react';
@@ -8,13 +7,15 @@ import React from 'react';
 import MusicScoreGrid from './MusicScoresGrid';
 import musicScoresPageStyles from './MusicScoresPage.styles';
 
+const musicScoresBg = 'https://storage.googleapis.com/storage.djin.dev/music/bg/music_score_bg.jpeg';
+
 const MusicScoresPage: React.FC = () => {
     const classes = musicScoresPageStyles();
     const musicScores = useMusicScores();
     const theme = useTheme();
     const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
     return (
-        <Background imageUrl={musicScoreBg} tint={false}>
+        <Background imageUrl={musicScoresBg} tint={false}>
             <Container maxWidth="lg">
                 <Typography variant={isSmall ? 'h3' : 'h2'} align="center" className={classes.pageTitle}>
                     Music Scores
--- src/data/musicScores.ts
+++ src/data/musicScores.ts
@@ -6,7 +6,7 @@ const Reclaimer: MusicScore = {
     date: DateTime.fromISO('2020-05-01T00:00:00-08:00'),
     parts: {
         [Instrument.PIANO]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5e9fe393950dee1e5f9e2105/1587536787717/Reclaimer+-+Score.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/preludeOfPurgatory/preludeOfPurgatory_piano.pdf',
     },
     trackUrl: 'https://soundcloud.com/djtaeyong/prelude-of-purgatory',
 };
@@ -16,7 +16,7 @@ const LamentingLoss: MusicScore = {
     date: DateTime.fromISO('2018-06-01T00:00:00-08:00'),
     parts: {
         [Instrument.PIANO]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b2b00cbf950b7ec2441a0b8/1529544910779/Lamenting_Loss+-+Score.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/lamentingLoss/lamentingLoss_piano.pdf',
     },
     trackUrl: 'https://soundcloud.com/djtaeyong/lamenting-loss',
 };
@@ -26,7 +26,7 @@ const Indeterminate: MusicScore = {
     date: DateTime.fromISO('2011-11-01T00:00:00-08:00'),
     parts: {
         [Instrument.ORCHESTRA]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b35eb1baa4a99232a630319/1530260251730/indeterminate_score.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/indeterminate/indeterminate_orchestra.pdf',
     },
     trackUrl: 'https://soundcloud.com/djtaeyong/indeterminate',
 };
@@ -36,19 +36,19 @@ const HybridDuality: MusicScore = {
     date: DateTime.fromISO('2011-04-01T00:00:00-08:00'),
     parts: {
         [Instrument.ORCHESTRA]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b35eb010e2e720f5f216392/1530260226290/Hybrid_Duality.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/hybridDuality/hybridDuality_orchestra.pdf',
         [Instrument.PIANO]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b35ebf9aa4a99232a6311fe/1530260475783/Hybrid_Duality_2_11+-+Piano.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/hybridDuality/hybridDuality_piano.pdf',
         [Instrument.VIOLIN1]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b35ec0e88251b0938038baa/1530260494797/Hybrid_Duality_2_11+-+Violin+1.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/hybridDuality/hybridDuality_violin1.pdf',
         [Instrument.VIOLIN2]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b35ec40aa4a99232a6316a1/1530260544756/Hybrid_Duality_2_11+-+Violin+2.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/hybridDuality/hybridDuality_violin2.pdf',
         [Instrument.VIOLA]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b35ec4f352f5320a97fdf16/1530260560177/Hybrid_Duality_2_11+-+Viola.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/hybridDuality/hybridDuality_viola.pdf',
         [Instrument.CELLO]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b35ec61575d1f7d68dfb1d6/1530260577333/Hybrid_Duality_2_11+-+Cello.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/hybridDuality/hybridDuality_cello.pdf',
         [Instrument.BASS]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b35ec73f950b745b91ebf84/1530260596079/Hybrid_Duality_2_11+-+Double+Bass.pdf',
+            'https://storage.googleapis.com/storage.djin.dev/music/docs/hybridDuality/hybridDuality_doubleBass.pdf',
     },
     trackUrl: 'https://soundcloud.com/djtaeyong/hybrid-duality',
 };
@@ -57,8 +57,7 @@ const Lost: MusicScore = {
     title: 'Lost (USOMC 3rd Place State Level Winner 2010)',
     date: DateTime.fromISO('2010-02-01T00:00:00-08:00'),
     parts: {
-        [Instrument.PIANO]:
-            'https://static1.squarespace.com/static/5b06683d89c172fcf6c30356/t/5b35eb63aa4a99232a630812/1530260324826/Lost.pdf',
+        [Instrument.PIANO]: 'https://storage.googleapis.com/storage.djin.dev/music/docs/lost/lost_piano.pdf',
     },
     trackUrl: 'https://soundcloud.com/djtaeyong/lost',
 };
