76250a9c32dee4ae6e5e851529ec9c8848bc5a46
Andrew Tsun <atsun@usc.edu>
2021-02-26 00:41:03 -0800

    Fix lint errors

--- src/types/martialArts/index.ts
+++ src/types/martialArts/index.ts
@@ -1,7 +1,6 @@
 import type { MartialArtsStudio } from './MartialArtsStudio';
 import MartialArtsStyleType from './MartialArtsStyleType';
 import type { MartialArtStyle } from './MartialArtStyle';
-import StudioIds from './StudioIds';
 
-export { MartialArtsStyleType, StudioIds };
+export { MartialArtsStyleType };
 export type { MartialArtsStudio, MartialArtStyle };
