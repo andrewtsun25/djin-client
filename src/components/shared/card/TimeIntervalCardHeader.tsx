14831db1558c28a93bfcbfd0697664d8957aed01
Andrew Tsun <atsun@usc.edu>
2021-02-26 07:37:50 -0800

    Add avatar stylng to current projects/jobs

--- src/components/shared/card/TimeIntervalCardHeader.tsx
+++ src/components/shared/card/TimeIntervalCardHeader.tsx
@@ -2,6 +2,8 @@ import { Avatar, CardHeader } from '@material-ui/core';
 import { DateTime } from 'luxon';
 import React from 'react';
 
+import StyledBadge from './StyledBadge';
+
 interface TimeIntervalCardHeaderProps {
     title: string;
     subtitle: string;
@@ -20,7 +22,22 @@ const TimeIntervalCardHeader: React.FC<TimeIntervalCardHeaderProps> = ({
     const subheader = endDate
         ? `${startDateMonth} ${startDateYear} - ${endDate.monthShort} ${endDate.year}, ${subtitle}`
         : `${startDateMonth} ${startDateYear} - Present, ${subtitle}`;
-    return <CardHeader title={title} subheader={subheader} avatar={<Avatar alt={title} src={logoUrl} />} />;
+    const baseAvatar: React.ReactNode = <Avatar alt={title} src={logoUrl} />;
+    const avatar: React.ReactNode = endDate ? (
+        baseAvatar
+    ) : (
+        <StyledBadge
+            overlap="circle"
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+            }}
+            variant="dot"
+        >
+            {baseAvatar}
+        </StyledBadge>
+    );
+    return <CardHeader title={title} subheader={subheader} avatar={avatar} />;
 };
 
 export default TimeIntervalCardHeader;
