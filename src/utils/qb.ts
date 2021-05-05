4ec13e25513e5c180950942f4a8eb6c847256e9f
Andrew Tsun <atsun@usc.edu>
2021-05-05 01:56:05 -0700

    Add documentation for query builder utils

--- src/utils/qb.ts
+++ src/utils/qb.ts
@@ -5,28 +5,45 @@ import { Config, ImmutableTree, JsonLogicTree, JsonTree, Utils as QbUtils } from
 const defaultQueryValue: JsonTree = { id: QbUtils.uuid(), type: 'group' };
 
 /**
- * Creates a default React Awesome Query Builder tree for a given config
+ * Creates a default query builder tree for a given config.
+ *
  * @param config - The configuration for the query builder
  */
 export function createDefaultTreeForConfig(config: Config): ImmutableTree {
     return QbUtils.checkTree(QbUtils.loadTree(defaultQueryValue), config);
 }
 
+/**
+ * Exports the internal state tree of the query builder into a JsonLogic tree that can be applied by JsonLogic to
+ * filter a collection.
+ *
+ * @param qbTree - The internal state of the query builder
+ * @param config - The configuration of the query builder
+ */
 export function exportTree(qbTree: ImmutableTree, config: Config): RulesLogic | undefined {
     const { logic, errors } = QbUtils.jsonLogicFormat(qbTree, config);
     if (!isNil(errors) && errors.length > 0) {
         throw new Error(errors.join(', '));
     }
-    if (isNil(logic)) {
-        return logic;
-    }
-    return logic as RulesLogic;
+    return isNil(logic) ? undefined : (logic as RulesLogic);
 }
 
+/**
+ * Imports a JsonLogic tree that can be applied to filter a collection to sett the internal state tree of the query
+ * builder.
+ *
+ * @param logicTree - The JsonLogic tree that can be applied by JsonLogic to filter a collection
+ * @param config - The configuration of the query builder
+ */
 export function importTree(logicTree: JsonLogicTree, config: Config): ImmutableTree {
     return QbUtils.loadFromJsonLogic(logicTree, config);
 }
 
+/**
+ * Checks too see if the internal state of the query builder is valid to perform any subsequent operations.
+ *
+ * @param qbTree - The internal state of the query builder
+ */
 export function isValidTree(qbTree: ImmutableTree): boolean {
     return QbUtils.isValidTree(qbTree);
 }
