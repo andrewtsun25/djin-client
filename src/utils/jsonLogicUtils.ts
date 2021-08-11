ce7cf5a0cc3a79958590cd96773d4b38e7ed0ac2
Andrew Tsun <atsun@usc.edu>
2021-08-11 00:42:30 -0700

    Fix bug for wrong parsing of dates from JSONLogic URL param string

--- src/utils/jsonLogicUtils.ts
+++ src/utils/jsonLogicUtils.ts
@@ -3,5 +3,6 @@ import { RulesLogic } from 'json-logic-js';
 import jsonLogic from './jsonLogic';
 
 export function filterCollectionWithLogicTree<T>(collection: T[], logicTree: RulesLogic): T[] {
+    console.log('Logic tree: ', logicTree);
     return collection.filter((item) => jsonLogic.apply(logicTree, item));
 }
