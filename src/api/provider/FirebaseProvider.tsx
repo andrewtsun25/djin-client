c5eba5d2a7c106614f6d203497fc0ea1370319e2
Andrew Tsun <atsun@usc.edu>
2021-11-07 00:33:17 -0700

    Make tests pass again

--- src/api/provider/FirebaseProvider.tsx
+++ src/api/provider/FirebaseProvider.tsx
@@ -1,6 +1,6 @@
 import { FirebaseContext } from 'api/context';
 import { config } from 'config/firebase';
-import { initializeApp } from 'firebase/app';
+import { deleteApp, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import React, { PropsWithChildren, useEffect, useState } from 'react';
 import { FirebaseServices } from 'types/api';
@@ -9,18 +9,16 @@ const FirebaseProvider: React.FC = ({ children }: PropsWithChildren<Record<strin
     const [services, setServices] = useState<FirebaseServices>({
         db: undefined,
     });
-    const [initialized, setInitialized] = useState<boolean>(false);
     useEffect(() => {
-        if (initialized) {
-            return;
-        }
         const app = initializeApp(config);
         const db = getFirestore(app);
         setServices({
             db: db,
         });
-        setInitialized(true);
-    }, [services, initialized]);
+        return (): void => {
+            deleteApp(app);
+        };
+    }, [services]);
     return <FirebaseContext.Provider value={services}>{children}</FirebaseContext.Provider>;
 };
 
